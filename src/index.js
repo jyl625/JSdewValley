4b8dea21889846cb39d1eb74a749fe74f637e388
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-29 00:12:06 -0700

    Create title header and set up game canvas/container

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//in here:
+//const MovingObject = require("./moving_object.js");
+//window.MovingObject = MovingObject;
+
+//in moving_object.js
+// function MovingObject() {
+//   // your code
+// }
+// module.exports = MovingObject;
+
+
+console.log("testing testing");
