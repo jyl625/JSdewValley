9a16a713de4f5aa1ee04bf7d21e062aa4e159c82
Jean Youn Lee <jean.youn.lee@gmail.com>
2021-10-30 19:48:56 -0700

    Delete some comments

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -13,9 +13,11 @@ class Game {
   gameLoop() {
     const step = () => {
 
+      //draw map
       this.gameWorld.draw(this.ctx);
 
       Object.values(this.gameWorld.gameObjects).forEach(object => {
+        object.x += 0.02;
         object.sprite.draw(this.ctx);
       })
 
@@ -28,42 +30,11 @@ class Game {
   }
 
   start() {
+    //create new instance of GameWorld
     this.gameWorld = new GameWorld(window.GameWorldMaps.FarmDefault);
     
     // start game loop
     this.gameLoop();
-    console.log("make sure to turn on game loop");
-
-    // //load farm map
-    // const farm = new Image();
-    // farm.onload = () => {
-    //   this.ctx.drawImage(farm, 0, 0);
-    // };
-    // farm.src = '././dist/assets/farm_dev.png';
-
-    // //place player
-    // const player = new GameObject({
-    //   x: 6, y: 4, src: '././dist/assets/character.png'
-    // });
-
-
-    // const potato = new PlantObject({
-    //   x: 2, y: 2, src: '././dist/assets/crops/potato.png'
-    // });
-
-
-    // const tomato = new PlantObject({
-    //   x: 3, y: 3, src: '././dist/assets/crops/tomato.png'
-    // });
-
-
-    // setTimeout (() => {
-    //   potato.sprite.draw(this.ctx);
-    //   tomato.sprite.draw(this.ctx);
-    //   player.sprite.draw(this.ctx);
-    //   console.log("setTimeout");
-    // }, 200);
-
 
   }
 }
